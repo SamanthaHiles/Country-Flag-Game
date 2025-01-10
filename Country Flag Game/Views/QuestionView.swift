85028c6d51b8234fe04a3f8f981c6311ba5496f8
Samantha Hiles <samih11@att.net>
2025-01-10 14:53:21 -0600

    Modified Question View

--- Country Flag Game/Views/QuestionView.swift
+++ Country Flag Game/Views/QuestionView.swift
@@ -12,32 +12,51 @@ struct QuestionView: View {
     var body: some View {
         VStack(spacing: 20) {
             if gameManager.playingGame {
-                
-            }
-            HStack {
-                ProgressBar(progress: 50)
+                HStack {
+                    Text("Country Flag Game")
+                        .fontWeight(.heavy)
+                        .padding()
+                    Spacer()
+                    Text("\(gameManager.index) out of \(gameManager.questions.count)")
+                        .padding()
+                }
+                ProgressBar(progress: gameManager.progress)
                 VStack(spacing: 10) {
-                    Text("Which Country's flag is this?")
-                    Image("Italy")
+                    Text("Which country's flag is this?")
+                        .font(.title)
+                    Image(gameManager.country)
                         .resizable()
                         .frame(width: 300, height: 200)
-                    AnswerRow(answer: Answer(text: "France", isCorrect: false))
-                    AnswerRow(answer: Answer(text: "Germany", isCorrect: false))
-                    AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
-                    AnswerRow(answer: Answer(text: "England", isCorrect: false))
+                    ForEach(gameManager.answerChoices) { answer in
+                        AnswerRow(answer: answer)
+                            .environmentObject(gameManager)
+                    }
                 }
-                CustomButton(text: "Next")
+                Button {
+                    gameManager.goToNextQuestion()
+                } label: {
+                    CustomButton(text: "Next", background: gameManager.answerSelected ?.yellow : .gray)
+                }
+                .disabled(!gameManager.answerSelected)
                 Spacer()
+            }
+            else {
                 Text("Country Flag Game")
-                    .foregroundColor(.yellow)
+                    .font(.title)
                     .fontWeight(.heavy)
-                Spacer()
-                Text("1 out of 3")
-                    .foregroundColor(.yellow)
+                Text("Congratulations! You've completed the game!")
+                Text("You scored \(gameManager.score) out of \(gameManager.questions.count)")
+                Button {
+                    gameManager.reset()
+                }label: {
+                    CustomButton(text: "Play Again")
+                }
             }
         }
-        .padding()
+        .foregroundColor(.yellow)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.cyan)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
