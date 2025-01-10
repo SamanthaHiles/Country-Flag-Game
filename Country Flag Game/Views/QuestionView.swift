9edd777b0b560782947d5ba15e01a1b48f50e83a
Samantha Hiles <samih11@att.net>
2025-01-10 13:23:55 -0600

    Created initial view

--- Country Flag Game/Views/QuestionView.swift
+++ Country Flag Game/Views/QuestionView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct QuestionView: View {
+    @EnvironmentObject var gameManager: GameManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            if gameManager.playingGame {
+                
+            }
+            HStack {
+                ProgressBar(progress: 50)
+                VStack(spacing: 10) {
+                    Text("Which Country's flag is this?")
+                    Image("Italy")
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                    AnswerRow(answer: Answer(text: "France", isCorrect: false))
+                    AnswerRow(answer: Answer(text: "Germany", isCorrect: false))
+                    AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
+                    AnswerRow(answer: Answer(text: "England", isCorrect: false))
+                }
+                CustomButton(text: "Next")
+                Spacer()
+                Text("Country Flag Game")
+                    .foregroundColor(.yellow)
+                    .fontWeight(.heavy)
+                Spacer()
+                Text("1 out of 3")
+                    .foregroundColor(.yellow)
+            }
+        }
+        .padding()
+        .background(.cyan)
     }
 }
 
